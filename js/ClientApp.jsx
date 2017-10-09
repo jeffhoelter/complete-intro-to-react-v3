01d70b038ff2837aa3503c038f746e88609555cf
jeffhoelter <jeffh@loar.net>
2017-10-09 11:44:01 -0400

    Added .babelrc to make the build actually work.

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
 
-const MyTitle = ({ color, title }) => {
-  return React.createElement("div", null, React.createElement("h1", { style: { color: color } }, title));
-};
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
 
-const MyFirstComponent = () => {
-  return React.createElement(
-    "div",
-    { id: "my-first-component" },
-    React.createElement(MyTitle, { title: "Game of Thrones Season 7", color: "Peru" }),
-    React.createElement(MyTitle, { title: "House of Cards", color: "GreenYellow" }),
-    React.createElement(MyTitle, { title: "Broad City", color: "YellowGreen" })
-  );
-};
-
-render(React.createElement(MyFirstComponent), document.getElementById("app"));
+render(<App />, document.getElementById("app"));

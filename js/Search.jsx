630eac42cbf970d21ded9989f5e2e805f98d5f7f
jeffhoelter <jeffh@loar.net>
2017-10-22 11:22:26 -0400

    Fixed eslint and babel, and added Search component.

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
 import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = () => (
-  <div className="search">
-    <div>{preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}</div>
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: "",
+  };
+
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="test"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0,
+            )
+            .map(show => <ShowCard key={show.imdbID} show={show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;

c77c99fb845193aafe6eee3b9834c2c264491fff
Jeff Hoelter <jeffh@loar.net>
2018-04-04 16:19:02 -0400

    Added flow types to Redux code.

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -1,9 +1,10 @@
+// @flow
 import { SET_SEARCH_TERM } from "./actions";
 
-export function setSearchTerm(searchTerm) {
+export function setSearchTerm(searchTerm: string) {
   return { type: SET_SEARCH_TERM, payload: searchTerm };
 }
 
-export function setSearchTerm2(searchTerm) {
+export function setSearchTerm2(searchTerm: string) {
   return { type: SET_SEARCH_TERM, payload: searchTerm };
 }

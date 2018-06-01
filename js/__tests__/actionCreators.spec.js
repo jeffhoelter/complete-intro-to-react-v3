401bef440513a6b701d7ca0bc54278ee2fc76b3c
Jeff Hoelter <jeffh@loar.net>
2018-06-01 09:23:09 -0400

    Added moxios test for getAPIDetails. Upgraded jest and yarn.

--- js/__tests__/actionCreators.spec.js
+++ js/__tests__/actionCreators.spec.js
@@ -1,22 +1,38 @@
 // @flow
 
-import { setSearchTerm, addApiData } from "../actionCreators";
+import moxios from "moxios";
+import { setSearchTerm, addApiData, getApiDetails } from "../actionCreators";
+
+const strangerThings = {
+  title: "Billions",
+  year: "2016–",
+  description:
+    'U.S. Attorney Chuck Rhoades goes after hedge fund king, Bobby "Axe" Axelrod in a battle between two powerful New York figures.',
+  poster: "b.jpg",
+  imdbID: "tt4270492",
+  trailer: "_raEUMLL-ZI",
+  rating: "4.5"
+};
 
 test("should return a valid search object", () => {
   expect(setSearchTerm("New York")).toMatchSnapshot();
 });
 
 test("should return a valid API data object", () => {
-  expect(
-    addApiData({
-      title: "Billions",
-      year: "2016–",
-      description:
-        'U.S. Attorney Chuck Rhoades goes after hedge fund king, Bobby "Axe" Axelrod in a battle between two powerful New York figures.',
-      poster: "b.jpg",
-      imdbID: "tt4270492",
-      trailer: "_raEUMLL-ZI",
-      rating: "4.5"
-    })
-  ).toMatchSnapshot();
+  expect(addApiData(strangerThings)).toMatchSnapshot();
+});
+
+test("should successfully getAPIDetails", (done: Function) => {
+  const dispatchMock = jest.fn();
+  moxios.withMock(() => {
+    getApiDetails(strangerThings.imdbID)(dispatchMock); // call the redux thunk
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({ status: 200, response: strangerThings }).then(() => {
+        expect(request.url).toEqual(`http://localhost:3000/${strangerThings.imdbID}`);
+        expect(dispatchMock).toBeCalledWith(addApiData(strangerThings));
+        done();
+      });
+    });
+  });
 });
